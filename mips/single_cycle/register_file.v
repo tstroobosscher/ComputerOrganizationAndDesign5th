01aae62a479da6e68cc5167ae9545b4505a8a434
Taylor Stroobosscher <stroobos@usc.edu>
2018-12-24 23:00:40 -0800

    started layout of the single cycle mips

---
+++ mips/single_cycle/register_file.v
@@ -0,0 +1,11 @@
+`timescale 1 ns / 100 ps
+
+/*
+	register file implemented based on the description 
+	provided in Computer Organization and Design, 5th
+*/
+
+module register_file()
+
+
+endmodule
