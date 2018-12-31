2123f567b1e20cb4c9cb1ccd83c807ca943b254b
Taylor Stroobosscher <stroobos@usc.edu>
2018-12-30 22:41:20 -0800

    compiled mips single cycle cpu completely

--- mips/single_cycle/main.v
+++ mips/single_cycle/main.v
@@ -16,14 +16,101 @@ module single_cycle_mips_32(clk, rst);
 	//
 	///////////////////////////////////////////////////////////////////////////
 
-	wire RegDst;
-	wire Branch;
-	wire MemRead;
-	wire MemToReg;
-	wire [1:0] AluOP;
-	wire MemWrite;
-	wire ALUSrc;
-	wire RegWrite;
+	localparam R_TYPE 		= 6'b000000;
+	localparam LOAD_WORD 	= 6'b100011;
+	localparam STORE_WORD 	= 6'b101011;
+	localparam BRANCH_EQ	= 6'b000100;
+
+	reg RegDst;
+	reg Branch;
+	reg MemRead;
+	reg MemToReg;
+	reg [1:0] AluOP;
+	reg MemWrite;
+	reg ALUSrc;
+	reg RegWrite;
+
+	wire [31:0] instruction;
+
+	always @(*) begin
+		case(instruction[31:26])
+			R_TYPE 	: begin
+				// R type uses both data addresses and a result address
+				RegDst <= 1'b1;
+				// second data reg goes to alu
+				ALUSrc <= 1'b0;
+				// alu result is sent back to the register file
+				MemToReg <= 1'b0;
+				// writing data bact to register file
+				RegWrite <= 1'b1;
+				// not reading from memory
+				MemRead <= 1'b0;
+				// not reading to memory
+				MemWrite <= 1'b0;
+				// increment PC;
+				Branch <= 1'b0;
+				// aluop 10 is R-type
+				AluOP <= 2'b10;
+			end
+			LOAD_WORD 	: begin
+				// writing mem data back to register file 
+				RegDst <= 1'b0;
+				// send the immediate value to the alu for addres offset
+				ALUSrc <= 1'b1;
+				// send RAM memory to register
+				MemToReg <= 1'b1;
+				// writing to register
+				RegWrite <= 1'b1;
+				// reading from memory
+				MemRead <= 1'b1;
+				// prevent memory corruption
+				MemWrite <= 1'b0;
+				// increment PC
+				Branch <= 1'b0;
+				// add address and immediate
+				AluOP <= 2'b00;
+			end
+			STORE_WORD 	: begin
+				// second data field not used
+				RegDst <= 1'bX;
+				// send immediate field to alu
+				ALUSrc <= 1'b1;
+				// not writing data back to register
+				MemToReg <= 1'bX;
+				// prevent register corruption
+				RegWrite <= 1'b0;
+				// prevent RAM corruption, this might not matter
+				MemRead <= 1'b0;
+				// write data to appropriate address
+				MemWrite <= 1'b1; 
+				// increment pc
+				Branch <= 1'b0;
+				// calculate address from immediate and PC+4
+				AluOP <= 2'b00;
+			end
+			BRANCH_EQ 	: begin
+				// no third field necessary
+				RegDst <= 1'bX;
+				// send second data field to alu
+				ALUSrc <= 1'b0;
+				// not writing back from memory to register
+				MemToReg <= 1'bX;
+				// not writing to register, prevent corruption
+				RegWrite <= 1'b0;
+				// not reading from register
+				MemRead <= 1'b0;
+				// not writing to memory
+				MemWrite <= 1'b0;
+				// branch if zero
+				Branch = 1'b1;
+				// check if zero subtract
+				AluOP <= 2'b01;
+			end
+			default begin
+				
+			end
+		endcase
+	end
 
 	///////////////////////////////////////////////////////////////////////////
 	//
@@ -59,11 +146,9 @@ module single_cycle_mips_32(clk, rst);
 	//
 	///////////////////////////////////////////////////////////////////////////	
 
-	wire [31:0] instruction;
-
 	inst_mem_64x32 inst_mem(
-		ra(program_counter),
-		rd(instruction)
+		.ra(program_counter[5:0]),
+		.rd(instruction)
 		);
 
 	///////////////////////////////////////////////////////////////////////////
@@ -83,14 +168,14 @@ module single_cycle_mips_32(clk, rst);
 	wire [31:0] read_data_2;
 
 	register_file reg_file(
-		clk(clk),
-		ra1(instruction[25:21]),
-		ra2(instruction[20:16]),
-		wa(write_address),
-		wd(write_data),
-		rd1(read_data_1),
-		rd2(read_data_1),
-		regwrite(RegWrite)
+		.clk(clk),
+		.ra1(instruction[25:21]),
+		.ra2(instruction[20:16]),
+		.wa(write_address),
+		.wd(write_data),
+		.rd1(read_data_1),
+		.rd2(read_data_1),
+		.regwrite(RegWrite)
 		);
 
 	///////////////////////////////////////////////////////////////////////////
@@ -116,8 +201,48 @@ module single_cycle_mips_32(clk, rst);
 	//	and outputs the alu control lines
 	///////////////////////////////////////////////////////////////////////////	
 
-	wire alu_control;
-	// TODO assign alucont
+	reg [3:0] alu_control;
+
+	localparam AND 	= 4'b0000;
+	localparam OR 	= 4'b0001;
+	localparam ADD 	= 4'b0010;
+	localparam SUB 	= 4'b0110;
+	localparam SLT 	= 4'b0111;
+	localparam NOR 	= 4'b1100;
+
+	wire [5:0] funct_control;
+	assign funct_control = instruction[5:0];
+
+	always @(*) begin
+
+		// default add (LW and SW)
+		if (~AluOP[1] && ~AluOP[0]) begin
+			alu_control <= ADD;
+		end
+
+		// default sub (beq)
+		else if (AluOP[0]) begin
+			alu_control <= SUB;
+		end
+
+		// r-type
+		else if(AluOP[1]) begin
+			if(funct_control[3:0] == 4'b0000)
+				alu_control <= ADD;
+			else if(funct_control[3:0] == 4'b0010)
+				alu_control <= SUB;
+			else if(funct_control[3:0] == 4'b0100)
+				alu_control <= AND;
+			else if(funct_control[3:0] == 4'b0101)
+				alu_control <= OR;
+			else if(funct_control[3:0] == 4'b1010)
+				alu_control <= SLT;
+			else
+				alu_control <= 4'bXXXX;
+		end
+		else
+			alu_control <= 4'bXXXX;
+	end
 
 	wire [31:0] alu_input_1;
 	wire [31:0] alu_input_2;
@@ -128,11 +253,11 @@ module single_cycle_mips_32(clk, rst);
 	assign alu_input_2 = ALUSrc ? sign_extended_immediate_16 : read_data_2;
 
 	alu alu(
-		alucont(alu_control),
-		rd1(alu_input_1),
-		rd2(alu_input_2),
-		res(alu_result),
-		zero(alu_zero)
+		.alucont(alu_control),
+		.rd1(alu_input_1),
+		.rd2(alu_input_2),
+		.res(alu_result),
+		.zero(alu_zero)
 		);
 
 	///////////////////////////////////////////////////////////////////////////
@@ -162,9 +287,10 @@ module single_cycle_mips_32(clk, rst);
 
 	wire [31:0] data_mem_data;
 
+	// lower 6 address bits are focused on because of the address size
 	data_mem_64x32 data_mem(
 		.clk(clk),
-		.addr(alu_result),
+		.addr(alu_result[5:0]),
 		.rd(data_mem_data),
 		.wd(read_data_2),
 		.memwrite(MemWrite),
