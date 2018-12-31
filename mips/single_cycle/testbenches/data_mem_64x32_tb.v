c18decbec7b6a18c6b15b73e994ce55472ac5d14
Taylor Stroobosscher <stroobos@usc.edu>
2018-12-30 17:11:55 -0800

    completed data mem test

--- mips/single_cycle/testbenches/data_mem_64x32_tb.v
+++ mips/single_cycle/testbenches/data_mem_64x32_tb.v
@@ -6,7 +6,7 @@
 
 module test_bench;
 
-	reg clk_tb = 1;
+	reg clk_tb;
 
 	// 100Mhz clock
 	always #5 clk_tb = ~clk_tb;
@@ -20,7 +20,7 @@ module test_bench;
 	///////////////////////////////////////////////////////////////////////////
 	
 
-	reg [6:0] addr_tb;
+	reg [5:0] addr_tb;
 	wire [31:0] rd_tb;
 	reg [31:0] wd_tb;
 	reg memwrite_tb;
@@ -42,99 +42,56 @@ module test_bench;
 	//
 	//
 	///////////////////////////////////////////////////////////////////////////
-	
-	initial begin
-		repeat(1000) @(posedge clk_tb);
-		$finish;
-	end
 
 	initial begin
 		$dumpfile("simulation.vcd");
-		$dumpvars;
+		$dumpvars(0, test_bench);
 	end
 
 	/*
 		evaluation  ($random)
-
-		state machine:
-			Start: initialize the registers, wait for start signal 
-			Write: write 64 random values to the memory, and temp them 
-			Read: read back those stored 64 values into temp2
-			Result: compare the temp1 and temp2
 	 */
 
-	reg [3:0] state;
 	reg start;
-	reg [7:0] i;
-	reg [31:0] test_mem [63:0];
-
-	localparam START = 0001;
-	localparam WRITE = 0010;
-	localparam READ = 0100;
-	localparam RESULT = 1000;
-
-	initial state = START;
-	initial start = 1'b0;
-
-	always @(posedge clk_tb) begin
-		case(state)
-			START 	:
-				begin
-
-					// RTL
-					if(~start) begin
-						addr_tb <= 32'b0;
-						wd_tb <= 32'b0;
-						memwrite_tb <= 1'b0;
-						memread_tb <= 1'b0;
-						i <= 0;
-					end
-					else if(start) begin
-						test_mem[addr_tb] <= wd_tb <= $random;
-						memwrite_tb <= 1'b1;
-					end
-
-					// NSL
-					if(start)
-						state <= WRITE;
-				end
-			WRITE 	:
-				begin
-
-					// RTL
-					if(addr_tb < 63) begin
-						addr_tb <= addr_tb + 1;
-						test_mem[addr_tb] <= wd_tb <= $random;
-						memwrite_tb <= 1'b1;
-					end
-					else if(addr_tb >= 63) begin
-						memwrite_tb <= 1'b0;
-					end
-
-					// NSL 
-					if(addr_tb == 63) begin
-						state <= READ;
-						addr_tb <= 0;
-					end
-				end
-			READ 	:
-				begin
-					
-				end
-
-
-		endcase
-	end
-
-	integer num_checks = 0;
+	reg [31:0] test_mem [0:63];
+	wire [31:0] expected_data;
 	integer num_errors = 0;
-	always @(posedge clk_tb) begin
-		num_checks = num_checks + 1;
-		if(expected_q != q_tb) begin
-			$display("ERROR: q value %0x does not match the expected value %0x at time %0fns", 
-				q_tb, expected_q, $realtime);
-			num_errors = num_errors + 1;
+
+	initial addr_tb = 0;
+	initial memwrite_tb = 0;
+	initial memread_tb = 0;
+	initial wd_tb = 0;
+
+	initial begin
+		clk_tb = 0;
+
+		// clock is starting high
+		// issues coming from write synchronization
+		// need to consider the whole clock cycle and the read/write 
+		// characteristics of the module
+		memwrite_tb = 1'b1;
+		repeat(64) begin
+			wd_tb = $random;
+			test_mem[addr_tb] = wd_tb;
+			#10;
+			addr_tb = addr_tb + 1;
 		end
+
+		memwrite_tb = 1'b0;
+		addr_tb = 0;
+		wd_tb = 32'bX;
+
+		memread_tb = 1'b1;
+		repeat(64) @(posedge clk_tb) begin
+			if(test_mem[addr_tb] != rd_tb) begin
+				$display("ERROR: q value %0x does not match the expected value %0x at address %d", 
+					rd_tb, test_mem[addr_tb], addr_tb);
+				//num_errors = num_errors + 1;
+			end
+			addr_tb = addr_tb + 1;
+		end
+
+		$finish;
 	end
 
 endmodule
